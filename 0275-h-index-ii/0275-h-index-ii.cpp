2c3c8bb7621986d83cf97444f17dc108502952cc
Anurag Haldey <aanurag2003haldey@gmail.com>
2024-02-01 19:07:06 +0530

    Time: 14 ms (56.34%), Space: 21 MB (12.33%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.cpp
+++ 0275-h-index-ii/0275-h-index-ii.cpp
@@ -4,22 +4,26 @@ public:
         return (c[mid]>=(c.size()-mid));
         
     }
+    
+    int BS(vector<int>&c,int l,int h,int &ans,int n){
+        if(l>h){
+            return ans;
+        }
+        int mid=l+(h-l)/2;
+        
+        if(check(mid,c)){
+            ans=max(ans,(n-mid));
+            return BS(c,l,mid-1,ans,n);
+        }
+        return BS(c,mid+1,h,ans,n);
+    }
     int hIndex(vector<int>& c) {
         int l=0,h=c.size()-1;
         int mid=l+(h-l)/2;
         int ans=INT_MIN;
         int n=c.size();
-        while(l<=h){
-            int mid=l+(h-l)/2;
-            
-            if(check(mid,c)){
-                ans=max(ans,(n-mid));
-                h=mid-1;
-            }else{
-                l=mid+1;
-            }
-        }
+        int p=BS(c,l,h,ans,n);
         
-        return ans==INT_MIN? 0:ans;
+        return p==INT_MIN? 0:p;
     }
 };
