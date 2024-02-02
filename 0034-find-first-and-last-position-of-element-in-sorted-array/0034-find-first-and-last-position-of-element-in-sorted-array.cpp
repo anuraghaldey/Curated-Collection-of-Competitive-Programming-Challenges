42e8670720b66e9efae2747af7d413dac178b1f0
Anurag Haldey <aanurag2003haldey@gmail.com>
2024-02-02 15:55:20 +0530

    Time: 3 ms (92.39%), Space: 16 MB (21.13%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
 int lb(vector<int>&a,int n,int x){
-    int l=0,h=n-1,ans=n;
+    int l=0,h=n-1,ans=-1;
 
     while(l<=h){
         int mid=l+(h-l)/2;
-        if(a[mid]>=x){
+        if(a[mid]==x){
             ans=mid;
             h=mid-1;
-        }else{
+        }else if(a[mid]<x){
             l=mid+1;
+        }else{
+            h=mid-1;
         }
     }
     return ans;
 }
 int ub(vector<int>&a,int n,int x){
-    int l=0,h=n-1,ans=n;
+    int l=0,h=n-1,ans=-1;
 
     while(l<=h){
         int mid=l+(h-l)/2;
-        if(a[mid]>x){
+        if(a[mid]==x){
             ans=mid;
-            h=mid-1;
-        }else{
             l=mid+1;
+        }else if(a[mid]<x){
+            l=mid+1;
+        }else{
+            h=mid-1;
         }
     }
     return ans;
@@ -32,14 +36,9 @@ int ub(vector<int>&a,int n,int x){
          int n=arr.size();
     int fi=lb(arr,n,k);
     int li=ub(arr,n,k);
-    vector<int>p;
-    if(fi!=n&&arr[fi]==k){
-       p.push_back(fi);
-        p.push_back(li-1);
-    }else{
-      p.push_back(-1);
-        p.push_back(-1);
-    }
-    return p;
+    vector<int>v;
+    v.push_back(fi);
+    v.push_back(li);
+    return v;
     }
 };
