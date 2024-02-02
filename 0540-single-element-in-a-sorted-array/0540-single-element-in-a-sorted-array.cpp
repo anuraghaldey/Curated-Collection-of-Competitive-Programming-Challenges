2cf66ae63aba5a6c312d5f6d3afe0dba97f24131
Anurag Haldey <aanurag2003haldey@gmail.com>
2024-02-03 01:12:35 +0530

    Time: 25 ms (21.92%), Space: 24.6 MB (26.43%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
     int BS(vector<int>& a, int l, int h) {
-        if (l == h) {
+        if(l>=h){
             return a[l];
         }
-
-        int mid = l + (h - l) / 2;
-
-        if (mid % 2 == 0) {
-            if (a[mid] == a[mid + 1]) {
-                return BS(a, mid + 1, h);
-            } else {
-                return BS(a, l, mid);
-            }
-        } else {
-            if (a[mid] == a[mid - 1]) {
-                return BS(a, mid + 1, h);
-            } else {
-                return BS(a, l, mid - 1);
-            }
+      
+        int mid=l+(h-l)/2;
+        if(a[mid]!=a[mid-1]&&a[mid]!=a[mid+1]){
+            return a[mid];
         }
+        if((mid%2&&a[mid-1]==a[mid])||!(mid%2)&&a[mid]==a[mid+1]){
+            return BS(a,mid+1,h);
+        }
+        return BS(a,l,mid-1);
+        
     }
 
     int singleNonDuplicate(vector<int>& a) {
-        return BS(a, 0, a.size() - 1);
+        if(a.size()==1){
+            return a[0];
+        }
+        if(a[1]!=a[0]){
+            return a[0];
+        }
+        if(a[a.size()-2]!=a[a.size()-1]){
+            return a[a.size()-1];
+        }
+        return BS(a, 1, a.size() - 2);
     }
 };
