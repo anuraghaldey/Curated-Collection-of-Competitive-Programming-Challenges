90965092ecf9bd9920d8834e3e0cbebb6d4b9bc4
Anurag Haldey <aanurag2003haldey@gmail.com>
2024-02-03 16:01:51 +0530

    Time: 6 ms (34.89%), Space: 11.1 MB (25.27%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
+    int BS(vector<int>&a,int l,int h,int &ans){
+        if(l>h){
+            return ans;
+        }
+        int mid=l+(h-l)/2;
+        if(a[mid]>a[mid-1]&&a[mid]>a[mid+1]){
+            ans=mid;
+            return ans;
+        }
+        return (((a[mid]<a[mid-1])&&(a[mid]<a[mid+1]))||(a[mid]>a[mid+1]))?
+            BS(a,l,mid-1,ans):BS(a,mid+1,h,ans);
+        
+        
+    }
     int findPeakElement(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 1 || nums[0] > nums[1]) {
+        int n=nums.size();
+        if(nums.size()==1||nums[0]>nums[1]){
             return 0;
         }
-        if (nums[n - 1] > nums[n - 2]) {
-            return n - 1;
+        
+        if(nums[n-1]>nums[n-2]){
+            return n-1;
         }
-
-        int l = 0, h = n - 1;  
-        int ans = -1;
-        while (l <= h) {
-            int mid = l + (h - l) / 2;
-
-            if ((mid == 0 || nums[mid] > nums[mid - 1]) && (mid == n - 1 || nums[mid] > nums[mid + 1])) {
-                ans = mid;
-                break;
-            }
-
-            if (mid > 0 && nums[mid - 1] > nums[mid]) {
-                h = mid - 1;
-            } else {
-                l = mid + 1;
-            }
-        }
-        return ans;
+        int ans=-1;
+        return BS(nums,1,n-2,ans);
     }
+    
 };
