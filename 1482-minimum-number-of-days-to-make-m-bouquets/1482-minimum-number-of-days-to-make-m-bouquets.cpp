bd6270620d0e2d97819f993c69f039a860dc9a07
Anurag Haldey <aanurag2003haldey@gmail.com>
2024-02-04 11:12:44 +0530

    Time: 115 ms (83.50%), Space: 69 MB (18.91%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,45 +1,36 @@
 class Solution {
 public:
-    bool check(int mid,vector<int>&b,int m,int k){
-        int n=0,ans=0;
+    int check(int mid,vector<int>&b,int m,int k){
+        int c=0;
+        int ans=0;
         for(int i=0;i<b.size();i++){
             if(b[i]<=mid){
-                n++;
-                if(n==k){
-                ans++;
-                n=0;
+                c++;
+            }else{
+                if(c){
+                    ans+=(c/k);
+                }
+                c=0;
             }
-            }
-            else{
-                n=0;
-            }
-            
         }
-        
+        ans+=(c/k);
         return ans>=m;
     }
-    int BS(vector<int>&b,int m,int k,int &ans,int l,int h){
-       
-        while(l<=h){
-            int mid=l+(h-l)/2;
-            
-            if(check(mid,b,m,k)){
-                ans=mid;
-                h=mid-1;
-            }else{
-                l=mid+1;
-            }
+    int BS(vector<int>&b,int l,int h,int m,int k,int &ans){
+        if(l>h){
+            return ans;
         }
-        return ans;
+        int mid=l+(h-l)/2;
+        if(check(mid,b,m,k)){
+            ans=mid;
+            return BS(b,l,mid-1,m,k,ans);
+        }
+        return BS(b,mid+1,h,m,k,ans);
     }
     int minDays(vector<int>& b, int m, int k) {
-        int l=1,ans=-1,h=1;
-        h=*max_element(b.begin(),b.end());
-        
-        if((m)>b.size()/k){
-            return -1;
-        }
-        
-        return BS(b,m,k,ans,l,h);
+        int l=*min_element(b.begin(),b.end());
+        int h=*max_element(b.begin(),b.end());
+        int ans=-1;
+        return BS(b,l,h,m,k,ans);
     }
 };
