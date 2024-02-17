06136636cc94dcacd995528bff07e0fcbedaa66d
Anurag Haldey <aanurag2003haldey@gmail.com>
2024-02-18 01:57:02 +0530

    Time: 12 ms (43.61%), Space: 9.4 MB (24.19%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int i=0,j=0;
+        int n=s.size();
+        int ans=0;
+        int mx=0;
+        unordered_map<char,int>mp;
+        while(j<n){
+            mp[s[j]]++;
+            mx=max(mx,mp[s[j]]);
+            if((j-i+1)-mx<=k){
+                ans=max(ans,j-i+1);
+            }else{
+                while((j-i+1)-mx>k){
+                    mp[s[i++]]--;
+                }
+            }
+            j++;
+            
+        }
+        return ans;
+    }
+};
