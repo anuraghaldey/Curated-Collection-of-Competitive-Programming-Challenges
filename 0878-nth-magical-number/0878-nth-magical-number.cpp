09a5878ee2944ce2b37357581f6e403c15e5b470
Anurag Haldey <aanurag2003haldey@gmail.com>
2024-04-09 16:40:54 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (40.00%) - LeetHub

---
+++ 0878-nth-magical-number/0878-nth-magical-number.cpp
@@ -0,0 +1,25 @@
+#define ll long long
+const int MOD = (1e9 + 7);
+class Solution {
+public:
+    ll count(ll mid,int a,int b){
+        ll tc=mid/a+mid/b-mid/((a*b)/__gcd(a,b));
+        return tc;
+    }
+    int nthMagicalNumber(int n, int a, int b) {
+        ll l=2,h=1e15;
+        ll ans=-1;
+        while(l<=h){
+            ll mid=l+(h-l)/2;
+            if(count(mid,a,b)>n){
+                h=mid-1;
+            }else if(count(mid,a,b)<n){
+                l=mid+1;
+            }else{
+                ans= mid;
+                h=mid-1;
+            }
+        }
+        return (ans%MOD+MOD)%MOD;
+    }
+};
