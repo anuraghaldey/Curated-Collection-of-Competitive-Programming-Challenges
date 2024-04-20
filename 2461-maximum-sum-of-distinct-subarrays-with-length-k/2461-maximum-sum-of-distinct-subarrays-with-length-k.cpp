a293e8a5ee14dcb55239809a0a5f386b26b90fe2
Anurag Haldey <aanurag2003haldey@gmail.com>
2024-04-21 03:01:57 +0530

    Time: 86 ms (97.28%), Space: 109.1 MB (5.08%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -1,27 +1,35 @@
+#include<iostream>
+using namespace std;
+
+
 class Solution {
 public:
+    
     long long maximumSubarraySum(vector<int>& a, int k) {
-        long long ans=0;
-        long long sum=0;
-        map<int,int>mp;
-        int n=a.size();
-        for(int i=0;i<k;i++){
-            mp[a[i]]++;
-            sum+=a[i];
+        vector<int> count(100100, 0);
+        long long ans = 0;
+        long long sum = 0;
+        int dist = 0;
+        int n = a.size();
+
+        for (int i = 0; i < k; i++) {
+            count[a[i]]++;
+            sum += a[i];
+            count[a[i]] == 1 ? dist++ : dist = dist;
         }
-        mp.size()==k?ans=max(ans,sum):ans=ans;
-        
-        for(int i=k;i<n;i++){
-            mp[a[i-k]]--;
-            mp[a[i]]++;
-            sum-=a[i-k];
-            sum+=a[i];
-            if(!(mp[a[i-k]])){
-                mp.erase(a[i-k]);
+        dist == k ? ans = max(ans, sum) : ans = ans;
+
+        for (int i = k; i < n; i++) {
+            count[a[i - k]]--;
+            count[a[i]]++;
+            sum -= a[i - k];
+            sum += a[i];
+            count[a[i - k]] == 0 ? dist-- : dist = dist;
+            if(a[i]!=a[i-k]){
+                count[a[i]] == 1 ? dist++ : dist = dist;   
             }
-            mp.size()==k?ans=max(ans,sum):ans=ans;
+            dist == k ? ans = max(ans, sum) : ans = ans;
         }
         return ans;
-        
     }
 };
