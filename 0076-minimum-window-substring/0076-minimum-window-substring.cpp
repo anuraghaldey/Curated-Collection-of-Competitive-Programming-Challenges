d4f13827d9a959d496cd7489c3903a4519564dcc
Anurag Haldey <aanurag2003haldey@gmail.com>
2024-06-17 00:39:22 +0530

    Time: 266 ms (5.05%), Space: 13.8 MB (5.02%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,36 +1,63 @@
 class Solution {
 public:
-    string minWindow(string s ,string t) {
-        int n=s.size();
-        int m=t.size();
-        if(n==0||m==0||n<m){
-            return "";
+    bool is_valid(map<char, int> &window, map<char, int> &tar){
+        for (auto &v : tar) {
+            if (window[v.first] < v.second) {
+                return false;
+            }
         }
-        unordered_map<char,int>mp;
-        for(int i=0;i<m;i++){
+        return true;
+    }
+    bool check(string &s, map<char, int> &mp, int k, int &st, int &en) {
+        int n = s.size();
+        map<char, int> mp1;
+        for (int i = 0; i < k; i++) {
+            mp1[s[i]]++;
+        }
+        if (is_valid(mp1, mp)) {
+            st = 0;
+            en = k - 1;
+            return true;
+        }
+        for (int i = k; i < n; i++) {
+            mp1[s[i - k]]--;
+            if (mp1[s[i - k]] == 0) {
+                mp1.erase(s[i - k]);
+            }
+            mp1[s[i]]++;
+            if (is_valid(mp1, mp)) {
+                st = i - k + 1;
+                en = i;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    
+
+    int bs(string &s, int l, int h, map<char, int> &mp, int &ans, int &st, int &en) {
+        if (l > h) {
+            return ans;
+        }
+        int mid = l + (h - l) / 2;
+        if (check(s, mp, mid, st, en)) {
+            ans = mid;
+            return bs(s, l, mid - 1, mp, ans, st, en);
+        }
+        return bs(s, mid + 1, h, mp, ans, st, en);
+    }
+
+    string minWindow(string s,string t) {
+        int m = s.size(), n = t.size();
+        int l = 1, h = m, ans = -1;
+        int st = -1, en = -1;
+        map<char, int> mp;
+        for (int i = 0; i < n; i++) {
             mp[t[i]]++;
         }
-        int i=0,j=0,ans=0;
-        int mnln=INT_MAX;
-        int count=0;
-        while(j<n){
-            if(mp.find(s[j])!=mp.end()&&mp[s[j]]>0){
-                count++;
-            }
-            mp[s[j]]--;
-            while(count==m){
-                if(mnln>j-i+1){
-                    mnln=j-i+1;
-                    ans=i;
-                }
-                mp[s[i]]++;
-                if(mp.find(s[i])!=mp.end()&&mp[s[i]]>0){
-                    count--;
-                }
-                i++;
-            }
-            j++;
-        }
-        return mnln==INT_MAX?"":s.substr(ans,mnln);
+        ans = bs(s, l, h, mp, ans, st, en);
+        if (ans == -1) return "";
+        return s.substr(st, en - st + 1);
     }
 };
