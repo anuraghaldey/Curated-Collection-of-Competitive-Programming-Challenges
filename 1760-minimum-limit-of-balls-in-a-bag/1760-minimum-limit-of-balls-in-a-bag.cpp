06a52ef09015b09b699b2f06a265a05853915e13
Anurag Haldey <aanurag2003haldey@gmail.com>
2024-06-20 01:55:12 +0530

    Time: 136 ms (20.96%), Space: 58.3 MB (51.10%) - LeetHub

--- 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.cpp
+++ 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-    int check(vector<int>& a, int mid, int k) {
-        int count = 0;
-        int n=a.size();
-        for (int i=0;i<n;i++) {
-            count += (a[i] - 1) / mid;
+    
+    int check(vector<int>&a,int mid,int k){
+        int cnt=0;
+        for(int i=0;i<a.size();i++){
+            if(a[i]%mid){
+                cnt+=(a[i]/mid);
+            }else{
+                cnt+=((a[i]/mid)-1);
+            }
         }
-        return count<=k;
+        return cnt<=k;
     }
-
-    int bs(vector<int>& a, int l, int h, int &ans, int k) {
-        if (l > h) {
-            return ans;
+    int bs(vector<int>&a,int l,int h,int &ans,int k){
+        if(l>h)return ans;
+        int mid=l+(h-l)/2;
+        if(check(a,mid,k)){
+            ans=mid;
+            return bs(a,l,mid-1,ans,k);
         }
-        int mid = l + (h - l) / 2;
-
-        if (check(a, mid, k)) {
-            ans = mid; 
-            return bs(a, l, mid - 1, ans, k); 
-        }
-        return bs(a, mid + 1, h, ans, k); 
+        return bs(a,mid+1,h,ans,k);
     }
-
     int minimumSize(vector<int>& a, int k) {
-        int l = 1, h = *max_element(a.begin(), a.end());
-        int ans = h;
-        return bs(a, l, h, ans, k);
+        int l=1,h=*max_element(a.begin(),a.end());
+        int ans=0;
+        return bs(a,l,h,ans,k);
     }
 };
