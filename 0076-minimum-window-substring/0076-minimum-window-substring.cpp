14f775b7ca93ccaa7399e9f49e3938cf0abdc3ff
Anurag Haldey <aanurag2003haldey@gmail.com>
2024-06-22 11:32:09 +0530

    Time: 224 ms (5.06%), Space: 12 MB (5.76%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,63 +1,55 @@
 class Solution {
 public:
-    bool is_valid(map<char, int> &window, map<char, int> &tar){
-        for (auto &v : tar) {
-            if (window[v.first] < v.second) {
-                return false;
+    int check(map<char, int>&windowfreq, char ch, map<char, int>&tfreq) {
+        for (auto v : tfreq) {
+            if (v.first == ch) {
+                if (windowfreq[v.first] + 1 < v.second) {
+                    return false;
+                }
+            } else {
+                if (windowfreq[v.first] < v.second) {
+                    return false;
+                }
             }
         }
-        return true;
+	    return true;
     }
-    bool check(string &s, map<char, int> &mp, int k, int &st, int &en) {
-        int n = s.size();
-        map<char, int> mp1;
-        for (int i = 0; i < k; i++) {
-            mp1[s[i]]++;
-        }
-        if (is_valid(mp1, mp)) {
-            st = 0;
-            en = k - 1;
-            return true;
-        }
-        for (int i = k; i < n; i++) {
-            mp1[s[i - k]]--;
-            if (mp1[s[i - k]] == 0) {
-                mp1.erase(s[i - k]);
-            }
-            mp1[s[i]]++;
-            if (is_valid(mp1, mp)) {
-                st = i - k + 1;
-                en = i;
-                return true;
-            }
-        }
-        return false;
-    }
-
-    
-
-    int bs(string &s, int l, int h, map<char, int> &mp, int &ans, int &st, int &en) {
-        if (l > h) {
-            return ans;
-        }
-        int mid = l + (h - l) / 2;
-        if (check(s, mp, mid, st, en)) {
-            ans = mid;
-            return bs(s, l, mid - 1, mp, ans, st, en);
-        }
-        return bs(s, mid + 1, h, mp, ans, st, en);
-    }
-
     string minWindow(string s,string t) {
-        int m = s.size(), n = t.size();
-        int l = 1, h = m, ans = -1;
-        int st = -1, en = -1;
-        map<char, int> mp;
-        for (int i = 0; i < n; i++) {
-            mp[t[i]]++;
+        
+
+        int n = s.size(), m = t.size();
+
+        map<char, int>tfreq;
+        for (int i = 0; i < m; i++) {
+            tfreq[t[i]]++;
         }
-        ans = bs(s, l, h, mp, ans, st, en);
-        if (ans == -1) return "";
-        return s.substr(st, en - st + 1);
+        int ans = n + 1;
+        int tail = 0, head = -1;
+        map<char, int>windowfreq;
+        int best_idx=-1;
+
+        while (tail < n) {
+            while (head + 1 < n && !check(windowfreq, s[head + 1], tfreq)) {
+                head++;
+                windowfreq[s[head]]++;
+            }
+            if (head + 1 < n ) {
+                int len = head - tail + 2;
+                if (len < ans) {
+                    ans = len;
+                    best_idx = tail;
+                }
+		    }
+            if (tail > head) {
+                tail++;
+                head = tail - 1;
+            }
+            else {
+                windowfreq[s[tail]]--;
+                tail++;
+            }
+        }
+
+        return ans==n+1?"":s.substr(best_idx,ans);
     }
 };
